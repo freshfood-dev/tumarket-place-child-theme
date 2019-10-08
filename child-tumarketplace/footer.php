1ff06026c0936e82e1652aa118ca520247f04dc8
rajadileepkumar <rajadileepkumar34@gmail.com>
2019-10-08 23:16:34 +0530

    tumarketplace|child-theme loaded wih footer 0.1 and typography and footer 0.1|ready for QA

--- child-tumarketplace/footer.php
+++ child-tumarketplace/footer.php
@@ -12,41 +12,21 @@
  */
 ?>
 </div><!-- #content -->
-<footer id="colophon" class="site-footer">
-	<div class="site-info">
-		<?php $blog_info = get_bloginfo( 'name' ); ?>
-		<?php if ( ! empty( $blog_info ) ) : ?>
-			<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-		<?php endif; ?>
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-			<?php
-			/* translators: %s: WordPress. */
-			printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
-			?>
-		</a>
-		<?php
-		if ( function_exists( 'the_privacy_policy_link' ) ) {
-			the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-		}
-		?>
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
+	<footer id="colophon" class="mini-footer">
+		<div class="container">
+			<?php $blog_info = get_bloginfo( 'name' ); ?>
+			<?php if ( ! empty( $blog_info ) ) : ?>
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+			<?php endif; ?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'footer',
-						'menu_class'     => 'footer-menu',
-						'depth'          => 1,
-					)
-				);
+					/* translators: %s: WordPress. */
+					printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
 				?>
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-	</div><!-- .site-info -->
+			</a>
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
