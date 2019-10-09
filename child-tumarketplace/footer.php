abcad5009e28bf1b9c01fc303df342ac29fcbbcc
rajadileepkumar <rajadileepkumar34@gmail.com>
2019-10-09 23:24:17 +0530

    tumarketplace|child-theme loaded wih footer 0.2 and widgets has been created|ready for QA

--- child-tumarketplace/footer.php
+++ child-tumarketplace/footer.php
@@ -13,18 +13,110 @@
 ?>
 </div><!-- #content -->
 	<footer id="colophon" class="mini-footer">
-		<div class="container">
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-				<?php
-					/* translators: %s: WordPress. */
-					printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
-				?>
-			</a>
-		</div><!-- .site-info -->
+		<div class="footer__support__navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+						<p class="footer__support__navigation__caption"><i class="fa fa-clock-o fa-lg"></i>WORKING TIME</p>
+						<p class="footer__support__navigation__sub__caption">Mon- Sun: 8.00 - 18.00</p>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+						<p class="footer__support__navigation__caption"><i class="fa fa-truck fa-lg"></i>FREE SHIPPING</p>
+						<p class="footer__support__navigation__sub__caption">On order over $199</p>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+						<p class="footer__support__navigation__caption"><i class="fa fa-money fa-lg"></i>MONEY BACK 100%</p>
+						<p class="footer__support__navigation__sub__caption">Within 30 Days after delivery</p>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+						<p class="footer__support__navigation__caption"><i class="fa fa-phone-square fa-lg"></i>PHONE: 0123456789</p>
+						<p class="footer__support__navigation__sub__caption">Order Online Now !</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer__navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer__navigaton__title">
+                            <h5>My Account</h5>
+                        </div>
+                        <nav>
+                            <ul class="footer__navigation__content">
+                                <li><a href="my-account.html" class="footer__navigation__content__links">My Account</a>
+                                </li>
+                                <li><a href="#" class="footer__navigation__content__links">Order History</a></li>
+                                <li><a href="wishlist" class="footer__navigation__content__links">Wish List</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Search Terms</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Returns</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer__navigaton__title">
+                            <h5>CUSTOMER SERVICE</h5>
+                        </div>
+                        <nav>
+                            <ul class="footer__navigation__content">
+                                <li><a href="my-account.html" class="footer__navigation__content__links">My Account</a>
+                                </li>
+                                <li><a href="#" class="footer__navigation__content__links">Order History</a></li>
+                                <li><a href="wishlist" class="footer__navigation__content__links">Wish List</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Search Terms</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Returns</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer__navigaton__title">
+                            <h5>PAYMENT & SHIPPING</h5>
+                        </div>
+                        <nav>
+                            <ul class="footer__navigation__content">
+                                <li><a href="my-account.html" class="footer__navigation__content__links">My Account</a>
+                                </li>
+                                <li><a href="#" class="footer__navigation__content__links">Order History</a></li>
+                                <li><a href="wishlist" class="footer__navigation__content__links">Wish List</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Search Terms</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Returns</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer__navigaton__title">
+                            <h5>PAYMENT & SHIPPING</h5>
+                        </div>
+                        <nav>
+                            <ul class="footer__navigation__content">
+                                <li><a href="my-account.html" class="footer__navigation__content__links">My Account</a>
+                                </li>
+                                <li><a href="#" class="footer__navigation__content__links">Order History</a></li>
+                                <li><a href="wishlist" class="footer__navigation__content__links">Wish List</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Search Terms</a></li>
+                                <li><a href="#" class="footer__navigation__content__links">Returns</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="copyright-area">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<span>Copyright ©</span>
+						<?php $blog_info = get_bloginfo( 'name' ); ?>
+						<?php if ( ! empty( $blog_info ) ) : ?>
+							<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						<?php endif; ?>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    	<img src="<?php echo get_stylesheet_directory_uri().'/assets/images/payment.png'; ?>" alt="payment" class="float-lg-right float-md-right float-sm-right float-xs-left"/>
+                	</div>
+				</div><!-- .site-info -->
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
--- child-tumarketplace/functions.php
+++ child-tumarketplace/functions.php
@@ -1,31 +1,58 @@
 <?php 
+	/*Load scripts and styles*/
 	add_action( 'wp_enqueue_scripts', 'child_tumarketplace_enqueue_styles' );
 	
 	function child_tumarketplace_enqueue_styles() {
  		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  		wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri().'/assets/css/bootstrap.min.css'); 
+ 		wp_enqueue_style( 'font-awesome-css', get_stylesheet_directory_uri().'/assets/css/font-awesome.min.css'); 
  		wp_enqueue_script('jquery');
  		wp_enqueue_script('bootstrap-js', get_stylesheet_directory_uri().'/assets/js/bootstrap.min.js', array('jquery'), '',true);
  		?>
  			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
  		<?php 
  	}
-    /*
- 	*function child_tumarketplace_javascript() {
-	 *   wp_enqueue_scripts('jquery');
-	  *  ?>
-	   * <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	
-		*<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>  
-	    <?php
-	}
-	add_action('wp_footer', 'child_tumarketplace_javascript'); 
 
-	add_action('wp_head', 'child_tumarketplace_styles'); 
-
-	function child_tumarketplace_styles() {
-		?>
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<?php 
+ 	
+ 	/**
+ 		* Add a sidebar.
+ 	*/
+	function child_tumarketplace_widgets_init() {
+	    if ( function_exists('register_sidebar') ) {
+		    $sidebar1 = array(
+		        'before_widget' => '<div class="widget %2$s">',
+		        'after_widget' => '</div>',
+		        'before_title' => '<h5 class="widgettitle">',
+		        'after_title' => '</h5>',        
+		        'name'=>__( 'Footer Widget-1', 'textdomain' ),  
+		    );  
+		    $sidebar2 = array(
+		        'before_widget' => '<div class="widget %2$s">',
+		        'after_widget' => '</div>',
+		        'before_title' => '<h5 class="widgettitle">',
+		        'after_title' => '</h5>',        
+		        'name'=>__( 'Footer Widget-2', 'textdomain' ),  
+		    );
+		    $sidebar3 = array(
+		        'before_widget' => '<div class="widget %2$s">',
+		        'after_widget' => '</div>',
+		        'before_title' => '<h5 class="widgettitle">',
+		        'after_title' => '</h5>',        
+		        'name'=>__( 'Footer Widget-3', 'textdomain' ),  
+		    );
+		    $sidebar4 = array(
+		        'before_widget' => '<div class="widget %2$s">',
+		        'after_widget' => '</div>',
+		        'before_title' => '<h5 class="widgettitle">',
+		        'after_title' => '</h5>',        
+		        'name'=>__( 'Footer Widget-4', 'textdomain' ),  
+		    );
+		     
+		    register_sidebar($sidebar1);
+		    register_sidebar($sidebar2);
+		    register_sidebar($sidebar3);
+		    register_sidebar($sidebar4);
+		}
 	}
-	*/
+	add_action( 'widgets_init', 'child_tumarketplace_widgets_init' );
  ?>
